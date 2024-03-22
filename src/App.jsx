851cdb45682cdb935aa56536494d2143b453900a
Wen Jie Lee <leewenjie.wjl@gmail.com>
2024-03-22 08:08:44 -0500

    Populated tfs and items, cannot be displayed yet

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,54 @@ import { useState, useEffect } from 'react'
 import { Scene } from "robot-scene"
 import useStore from "./Store.js"
 import { MeshLookupTable } from './MeshLookup.js'
+import {parseUrdfForJoints, parseUrdfForLinks} from "./tfs.js"
+import urdf from "./MeshLoaders/Stretch/stretch_main.urdf?raw"
 import './App.css'
 
-function App() {
-  const args = {
-    "tfs": {
-      "test_tf": {
-        "frame": "world",
-        "position": { "x": 0, "y": 0, "z": 0 },
-        "rotation": { "w": 1, "x": 0, "y": 0, "z": 0 },
-        "scale": { "x": 1, "y": 1, "z": 1 }
-      }
-    },
-    "items": {
-      "test_item": {
-        shape: "stretch_base",
-        name: "test_item",
-        frame: "test_tf",
-        position: { "x": 0, "y": 0, "z": 0 }, //position relative to frame
-        rotation: { "w": 1, "x": 0, "y": 0, "z": 0 }, //rotation relative to frame
-        color: { "r": 255, "g": 0, "b": 0, "a": 1 },
-        scale: { "x": 1, "y": 1, "z": 1 },
-        highlighted: "False"
-      }
-    }
 
-  }
-  useEffect(() => {
-    useStore.setState(args);
+function App() {
+  const [tfs, setTfs] = useState(null)
+  const [items, setItems] = useState(null)
+
+  useEffect( ()=>{
+    const items = parseUrdfForLinks(urdf)
+    setItems(items) 
   }, []);
 
+  useEffect(() => {
+    if (items) {
+      const tfs = parseUrdfForJoints(urdf, items)
+      setTfs(tfs);
+    }
+  }, [items]); 
+
+  useEffect(()=>{
+    tfs&& useStore.getState().updateTfs(tfs);
+    console.log(tfs)
+  }, [tfs])
+  useEffect(()=>{
+    items&& useStore.getState().updateItems(items);
+    console.log(items)
+  }, [items])
+
+  
+  const otherArgs = {
+    displayTfs: true,
+    displayGrid: true,
+    isPolar: false,
+    backgroundColor: "#d0d0d0",
+    planeColor: "#a8a8a8",
+    highlightColor: "#ffffff",
+    plane: -0.75,
+    fov: 60,
+    ar: false,
+    vr: false,
+    onPointerMissed: () => console.log("Missed Click"),
+  }
   return (
     <div id="scene-container">
-      <Scene store={useStore} meshLookup={MeshLookupTable} otherProps ={args}/>
-    </div>
+      <Scene store={useStore} meshLookup={MeshLookupTable} {...otherArgs}/>
+    </div>    
   )
 }
 
--- src/Store.js
+++ src/Store.js
@@ -1,15 +1,23 @@
-import {create }from 'zustand';
-
-const useStore = create((set,get) => ({
+import { create } from 'zustand';
+import { Timer} from './Timer'
+const useStore = create((set, get) => ({
   ip: '',
-  blocks: {}, 
-  Start:{},
-  tfs:{},
-  items:{},
+  blocks: {},
+  Start: {},
+  lines:{},
+  hulls:{},
+  texts:{},
+  points:{},
+  widgets:{},
+  clock: new Timer(),
+  tfs: {
+  },
+  items: {
+  },
 
   setIp: (ip) => set({ ip }),
-  addBlock: (id, json) => set((state) => ({ 
-    blocks: { ...state.blocks, [id]: json}
+  addBlock: (id, json) => set((state) => ({
+    blocks: { ...state.blocks, [id]: json }
   })),
   removeBlock: (id) => set((state) => {
     // Create a new object excluding the block with the given id
@@ -18,26 +26,27 @@ const useStore = create((set,get) => ({
         acc[currentId] = state.blocks[currentId];
       }
       return acc;
-    }, {}); 
- 
+    }, {});
+
     // Update the state with the new blocks object
     return { blocks: newBlocks };
   }),
   updateBlock: (id, updatedJson) => set((state) => ({
-    blocks: { ...state.blocks, [id]: updatedJson  }
+    blocks: { ...state.blocks, [id]: updatedJson }
   })),
+  updateTfs: (newTfs) => set({ tfs: newTfs }),
+  updateItems: (newItems) => set({ items: newItems }),
   getBlock: (id) => get().blocks[id],
 
   getBlocksByType: (type) => {
     const blocks = get().blocks;
     return Object.values(blocks).filter(block => block.type === type)[0];
-  }, 
+  },
 
 
 
-
-  addBlocktoStart: (id, json) => set((state) => ({ 
-    Start: { ...state.Start, [id]: json}
+  addBlocktoStart: (id, json) => set((state) => ({
+    Start: { ...state.Start, [id]: json }
   })),
   removeBlockfromStart: (id) => set((state) => {
     // Create a new object excluding the block with the given id
@@ -47,11 +56,11 @@ const useStore = create((set,get) => ({
       }
       return acc;
     }, {});
- 
+
     // Update the state with the new blocks object
     return { Start: newBlocks };
   }),
-  
+
 }));
 
 export default useStore;
